bd0711243bf9797ff365e209d9ab743ff2043f72
DESKTOP-9NUMGEK\yangyuxin <592996903@qq.com>
2022-08-14 17:54:45 +0800

    SpringBoot的启动类文件

---
+++ src/main/java/com/atyyx/mybatis_plus_datasource/MybatisPlusDatasourceApplication.java
@@ -0,0 +1,14 @@
+package com.atyyx.mybatis_plus_datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MybatisPlusDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDatasourceApplication.class, args);
+    }
+
+}
